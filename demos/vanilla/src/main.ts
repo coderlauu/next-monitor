9af639ae20809ab8c68d734e3faf1416adc040b4
Keung-Lau <1552951291@qq.com>
2025-06-08 22:48:16 +0800

    feat(vanilla): ✨  project init & testing import of child package

---
+++ demos/vanilla/src/main.ts
@@ -0,0 +1,36 @@
+import './style.css'
+
+/**
+ * 引入 SDK
+ */
+import { init } from '@next-monitor/monitor-sdk-browser'
+
+import viteLogo from '/vite.svg'
+
+import { setupCounter } from './counter.ts'
+import typescriptLogo from './typescript.svg'
+
+// 初始化 SDK
+init({
+    dsn: 'http://localhost:8000/',
+})
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
